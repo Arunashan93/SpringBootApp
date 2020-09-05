2d5bbb85669ae95a6f78c88b1b6a92b7f7ad9358
Arunashan93 <69149874+Arunashan93@users.noreply.github.com>
2020-09-05 15:20:43 +0530

    Delete config.js

--- target/classes/static/js/config.js
+++
@@ -1,8 +0,0 @@
-window._config = {
-    cognito: {
-        userPoolId: 'us-east-1_SqKFLIfpp', // e.g. us-east-2_uXboG5pAb
-        region: 'us-east-1', // e.g. us-east-2
-		clientId: '6mni63mi9rrjm9r4g5v4juf414' //is this used anywhere?
-    },
-};
-
